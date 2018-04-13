e9b502f6eb11f3e9da3af2e4d91d5560053c15e3
bbrown683 <bbrown683@live.com>
2018-04-13 17:34:27 -0500

    Adding code to save progress.

--- src/AltBitClient.java
+++ src/AltBitClient.java
@@ -1,20 +1,18 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 
 public class AltBitClient {
     private DatagramSocket socket;
     private InetAddress serverAddress;
     private int serverPort;
+    private boolean bit;
+    private boolean transferComplete;
 
     AltBitClient(InetAddress serverAddress, int serverPort) {
         this.serverAddress = serverAddress;
         this.serverPort = serverPort;
+        this.bit = false;
+        this.transferComplete = false;
         try {
             socket = new DatagramSocket();
         } catch (SocketException e) {
@@ -23,23 +21,35 @@ public class AltBitClient {
     }
 
     private void printPacketContents(DatagramPacket packet) {
-        // Not formatted as of yet.
-        System.out.println(packet.getData());
+        if(packet != null) {
+            System.out.println("====================================");
+            System.out.println("Source: " + packet.getAddress());
+            System.out.println("Port:" + packet.getPort());
+            System.out.println("Time:" + System.currentTimeMillis());
+            System.out.println("Payload: " + decode(packet.getData()));
+            System.out.println("====================================");
+        }
     }
 
-    private DatagramPacket receive() {
+    private DatagramPacket receive(DatagramPacket old) {
         byte[] buffer = new byte[1024];
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
         try {
             socket.receive(packet);
+        } catch (SocketTimeoutException e) {
+            send(packet);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return packet;
     }
 
-    private void send(byte[] buffer, InetAddress address, int port) {
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
+    private String decode(byte[] data) {
+        return new String(data, 0, data.length);
+    }
+
+    private void send(DatagramPacket packet) {
         try {
             socket.send(packet);
         } catch(IOException e) {
@@ -47,7 +57,70 @@ public class AltBitClient {
         }
     }
 
-    void run() {
-        send("FILE".getBytes(), serverAddress, serverPort);
+    private DatagramPacket interpret(DatagramPacket incoming, DatagramPacket resend) {
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        packet.setAddress(incoming.getAddress());
+        packet.setPort(incoming.getPort());
+
+        // Get first 4 bytes of payload. This will give us the ACK code (0 or 1).
+        String bitChar = decode(incoming.getData()).substring(0, 4);
+
+        // Check to see if packet is a FIN. If so we can stop.
+        String command = decode(incoming.getData()).substring(4, 7);
+        if(command.equals("FIN")) {
+            this.transferComplete = true;
+            return null;
+        }
+
+        if(bitChar.equals("ACK0")) {
+            // If these do not match up something is out of sync. Resend packet.
+            if(bit)
+                return resend;
+            // Flip bit and add payload.
+            String payload = "ACK0|";
+            packet.setData(payload.getBytes());
+            packet.setLength(payload.getBytes().length);
+            bit = true;
+        }
+        else {
+            // If these do not match up something is out of sync. Resend packet.
+            if(!bit)
+                return resend;
+            // Flip bit and add payload.
+            String payload = "ACK1|";
+            packet.setData(payload.getBytes());
+            packet.setLength(payload.getBytes().length);
+            bit = false;
+        }
+        return packet;
+    }
+
+    void run(String filename) {
+        // Send initial request for file download.
+        byte[] buffer = new byte[1024];
+        DatagramPacket requestPacket = new DatagramPacket(buffer, buffer.length);
+        String payload = "ACK0FILE" + filename + "|";
+        requestPacket.setData(payload.getBytes());
+        requestPacket.setLength(payload.getBytes().length);
+        requestPacket.setAddress(serverAddress);
+        requestPacket.setPort(serverPort);
+        bit = true;
+        printPacketContents(requestPacket);
+        send(requestPacket);
+
+        // Set the resend packet to the original packet.
+        DatagramPacket resendPacket = requestPacket;
+        while(true) {
+            DatagramPacket incomingPacket = receive(resendPacket);
+            printPacketContents(incomingPacket);
+            DatagramPacket outgoingPacket = interpret(incomingPacket, resendPacket);
+            printPacketContents(outgoingPacket);
+            if(transferComplete)
+                break;
+            send(outgoingPacket);
+            // Update resend packet each time we send something.
+            resendPacket = outgoingPacket;
+        }
     }
 }
--- src/AltBitServer.java
+++ src/AltBitServer.java
@@ -1,22 +1,15 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
-import java.nio.Buffer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
+import java.net.*;
+import java.util.LinkedList;
 
 public class AltBitServer {
     private DatagramSocket socket;
-    private int port;
+    private boolean bit;
+    private LinkedList<byte[]> payloads;
 
     AltBitServer(int port) {
-        this.port = port;
+        this.bit = false;
+        this.payloads = new LinkedList<>();
         try {
             socket = new DatagramSocket(port);
         } catch (SocketException e) {
@@ -24,25 +17,40 @@ public class AltBitServer {
         }
     }
 
-    private String fileToString(String filename) {
+    private void fileToChunks(String filename) {
         try {
-            return new String(Files.readAllBytes(Paths.get(filename)));
+            // Adds a file into a series of chunks with a size
+            // of 1024 bytes (the size of our socket buffer.
+            File file = new File(filename);
+            long length = file.length();
+            InputStream is = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int offset = 0;
+            do {
+                int pos = is.read(buffer, 0, 1024);
+                offset += pos;
+                // Add to stack and reset for next run.
+                payloads.offer(buffer);
+                buffer  = new byte[1024];
+            } while(offset < length);
+            is.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return new String();
     }
 
     private void printPacketContents(DatagramPacket packet) {
-        // Not formatted as of yet.
-        if(packet != null) {
+        if(!packet.getData().toString().isEmpty()) {
             System.out.println("====================================");
             System.out.println("Source: " + packet.getAddress());
-            System.out.println("Payload: " + packet.getData());
+            System.out.println("Port:" + packet.getPort());
+            System.out.println("Payload: " + decode(packet.getData()));
             System.out.println("====================================");
         }
-        else
-            System.out.println("Uninitialized Packet!");
+    }
+
+    private String decode(byte[] data) {
+        return new String(data, 0, data.length);
     }
 
     private DatagramPacket receive() {
@@ -50,14 +58,16 @@ public class AltBitServer {
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
         try {
             socket.receive(packet);
+        } catch (SocketTimeoutException e) {
+            // Resend packet if it times out.
+            send(packet);
         } catch (IOException e) {
             e.printStackTrace();
         }
         return packet;
     }
 
-    private void send(byte[] buffer, InetAddress address, int port) {
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, port);
+    private void send(DatagramPacket packet) {
         try {
             socket.send(packet);
         } catch(IOException e) {
@@ -65,10 +75,71 @@ public class AltBitServer {
         }
     }
 
+    private DatagramPacket interpret(DatagramPacket incoming, DatagramPacket resendPacket) {
+        byte[] buffer = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        packet.setAddress(incoming.getAddress());
+        packet.setPort(incoming.getPort());
+
+        // Get first byte of payload. This will give us the ACK code (0 or 1).
+        String bitChar = decode(incoming.getData()).substring(0, 4);
+
+        // Get next 4 bytes to see if this is a file request.
+        String command = decode(incoming.getData()).substring(4, 8);
+        System.out.println(command);
+        if(command.equals("FILE")) {
+            // I am using the pipe ("|") character to signify the end of data in a buffer since Java
+            // does not use whitespace.
+            String payload = decode(incoming.getData());
+            String cleaned = payload.substring(8, payload.indexOf("|"));
+            fileToChunks(cleaned);
+        }
+
+        if(bitChar.equals("ACK0")) {
+            // Does not match what came in.
+            // Resend packet.
+            if(bit)
+                return resendPacket;
+            String payload = "ACK1";
+            if(payloads.isEmpty())
+                payload += "FIN";
+            else
+                payload += decode(payloads.poll());
+            payload += "|";
+            bit = true;
+            packet.setData(payload.getBytes());
+            packet.setLength(payload.getBytes().length);
+        }
+        else {
+            // Does not match what came in.
+            // Resend packet.
+            if(!bit)
+                return resendPacket;
+            String payload = "ACK0";
+            if(payloads.isEmpty())
+                payload += "FIN";
+            else
+                payload += decode(payloads.poll());
+            // Signifies end of data in the buffer.
+            payload += "|";
+            // Flip bit.
+            bit = false;
+            // Send data.
+            packet.setData(payload.getBytes());
+            packet.setLength(payload.getBytes().length);
+        }
+        return packet;
+    }
+
     void run() {
+        DatagramPacket resendPacket = new DatagramPacket(new byte[1024], 1024);
         while(true) {
-            DatagramPacket packet = receive();
-            printPacketContents(packet);
+            DatagramPacket incomingPacket = receive();
+            printPacketContents(incomingPacket);
+            DatagramPacket outgoingPacket = interpret(incomingPacket, resendPacket);
+            printPacketContents(outgoingPacket);
+            send(outgoingPacket);
+            resendPacket = outgoingPacket;
         }
     }
 }
